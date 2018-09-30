f7babedd4e543c9c7e8236b99a10f22796d8e709
scottyd_14 <scottyd_14@hotmail.com>
2018-09-30 20:23:40 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,8 +2,14 @@
 # See README.md for instructions on how to do this
 
 def fizzbuzz(num)
-  
-  
+  if num % 3 == 0
+    "Fizz"
+  elsif num % 5 == 0
+    "Buzz"
+  elsif num % 3 == 0 && num % 5 == 0  
+    "fizzbuzz"
+  else 
+    nil
 end
 
 
